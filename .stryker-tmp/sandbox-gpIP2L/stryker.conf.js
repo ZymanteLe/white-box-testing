e0f973cb1a537969674178aaf526a51874aa3e03
Lukoševiciene <z.lukoseviciene@accenture.com>
2025-04-23 22:46:53 +0300

    Remove .stryker-tmp folder from repository

--- .stryker-tmp/sandbox-gpIP2L/stryker.conf.js
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-module.exports = {
-    mutate: ['commentController.js'],
-    mutator: 'javascript',
-    testRunner: 'jest',
-    reporters: ['html', 'clear-text', 'progress'],
-    coverageAnalysis: 'off'
-  };
-  
